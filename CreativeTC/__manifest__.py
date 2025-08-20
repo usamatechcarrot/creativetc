48357c586cb9858d5554213dc0e79806ea996323
Usama Ali <usamaali8981@gmail.com>
2025-08-20 10:16:00 +0400

    Add capacity field.

---
+++ CreativeTC/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Stock Location Progress Bar',
+    'version': '18.0',
+    'category': 'Inventory',
+    'summary': 'Show progress bar of stock usage in each location',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_location_views.xml',
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
