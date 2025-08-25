bb0ff7eb36ab3dce622070b2935b115c227abbe8
Usama Ali <usamaali8981@gmail.com>
2025-08-25 13:26:14 +0400

    fixes.

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0',
     'category': 'Inventory',
     'summary': 'Show progress bar of stock usage in each location',
-    'depends': ['stock','todo'],
+    'depends': ['stock','todo', 'base'],
     'data': [
         'views/stock_location_views.xml',
         'views/product_views.xml',
