aa383b0cffe7e7f1e06e0f5325201ce57a0d331c
Usama Ali <usamaali8981@gmail.com>
2025-08-25 12:21:36 +0400

    Usama Changes for date

--- CreativeTC/models/project.task.py
+++ CreativeTC/models/project.task.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = "project.task"
 
-    date_assigned = fields.date(
-        string="Date the task was assigned",
-        help="Define the date on which the task was assigned. <Dinesh>"
+    date_assigned = fields.Date(
+        string="Assigned Date",
+        help="Date when the task was assigned. <Dinesh>"
     )
