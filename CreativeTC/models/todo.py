cabe03177388144c5baa942ec19fd8be834ec984
Usama Ali <usamaali8981@gmail.com>
2025-08-25 13:03:46 +0400

    fixes.

--- CreativeTC/models/todo.py
+++ CreativeTC/models/todo.py
@@ -1,11 +1,5 @@
 from odoo import models, fields
 
 class TodoTask(models.Model):
-    _name = "todo.task"
-    _description = "Todo Task"
-
-    name = fields.Char("Title", required=True)
-    description = fields.Text("Description")
-
     # 👉 New text field
     notes = fields.Text(string="Notes")  
