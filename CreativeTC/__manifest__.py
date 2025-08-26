b1b598cb850c602d03582376591148d1f79aaff9
dineshtechcarrot <dinesh.rampal@techcarrot.ae>
2025-08-26 13:12:51 +0400

    changed dep

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0',
     'category': 'Inventory',
     'summary': 'Show progress bar of stock usage in each location',
-    'depends': ['stock', 'project', 'project_todo','hr'],
+    'depends': ['stock', 'project', 'project_todo','hr','hr_attendance'],
     
     'data': [
         'views/stock_location_views.xml',
