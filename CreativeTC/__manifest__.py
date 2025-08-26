f9c92042f8de79a4475598474e8bca84ca01ee40
Usama Ali <usamaali8981@gmail.com>
2025-08-26 10:45:34 +0400

    Contact changes

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -3,13 +3,13 @@
     'version': '18.0',
     'category': 'Inventory',
     'summary': 'Show progress bar of stock usage in each location',
-    'depends': ['stock', 'project', 'project_todo', 'hr', 'contacts'],
+    'depends': ['stock', 'project', 'project_todo', 'contacts'],
     
     'data': [
         'views/stock_location_views.xml',
         'views/product_views.xml',
         'views/todo_task_views.xml',
-        'views/hr_employees_form.xml',
+        'views/res_partner_view.xml',
     ],
     'installable': True,
     'application': False,
