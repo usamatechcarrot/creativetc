ae4af3595be9657fb176b2efcfbd08638ee1ee3e
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-26 11:52:12 +0400

    New code
    we removed ratings and adding the preferred location field now

--- CreativeTC/models/res_partner.py
+++ CreativeTC/models/res_partner.py
@@ -3,10 +3,7 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-#     partner_rating = fields.Selection([
-#         ('1', '⭐'),
-#         ('2', '⭐⭐'),
-#         ('3', '⭐⭐⭐'),
-#         ('4', '⭐⭐⭐⭐'),
-#         ('5', '⭐⭐⭐⭐⭐'),
-#     ], string="Ratingssss")
+    preferred_location = fields.Selection([
+        ('india', 'India'),
+        ('uae', 'UAE'),
+    ], string="Preferred Location")
