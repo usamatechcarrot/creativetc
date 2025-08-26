401f35cf6e88fb45dc4151a997be0b9402e9a54a
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-26 15:44:42 +0400

    run
    run

--- CreativeTC/models/res_partner.py
+++ CreativeTC/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    preferred_location = fields.Selection([
-        ('india', 'India'),
-        ('uae', 'UAE'),
-    ], string="Preferred Location")
+    # preferred_location = fields.Selection([
+    #     ('india', 'India'),
+    #     ('uae', 'UAE'),
+    # ], string="Preferred Location")
