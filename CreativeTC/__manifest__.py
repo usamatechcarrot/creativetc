97256ddf488e273c4ca2a0ac5c39cfd14f28943e
dineshtechcarrot <dinesh.rampal@techcarrot.ae>
2025-08-27 15:52:11 +0400

    restore

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -3,14 +3,12 @@
     'version': '18.0',
     'category': 'Inventory',
     'summary': 'Show progress bar of stock usage in each location',
-    'depends': ['stock', 'project', 'project_todo','base','hr'],
-    
+    'depends': ['stock', 'project', 'project_todo','hr','hr_attendance'],    
     'data': [
         'views/stock_location_views.xml',
         'views/product_views.xml',
         'views/todo_task_views.xml',
         'views/hr_employee_views.xml',
-        'security/ir.model.access.csv',
      ],
     'installable': True,
     'application': False,
---
+++ CreativeTC/models/hr_employee.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class HREmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    attendance_ids = fields.One2many(
+        'hr.attendance',
+        'employee_id',
+        string="Attendance Records"
+    )
