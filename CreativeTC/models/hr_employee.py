33655b15beaa5b8b9f10e531708e98bfa5a60a6e
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-27 09:06:08 +0400

    HR-Compute Feature
    HR-Compute Feature

--- CreativeTC/models/hr_employee.py
+++ CreativeTC/models/hr_employee.py
@@ -1,7 +1,24 @@
 
-from odoo import models, fields
+from datetime import date
+from odoo import models, fields, api
 
 class Employee(models.Model):
     _inherit = 'hr.employee'
 
     test_field = fields.Char(string="This is a test field")
+ # You can type the employee's joining date here
+    joining_date = fields.Date(string="Joining Date")
+
+      # Auto-computed in years from joining_date to today
+    experience_years = fields.Integer(
+        string="Experience (Years)",
+        compute="_compute_experience_years",
+        store=True  # set to False if you want it to always compute on the fly
+    )
+     @api.depends('joining_date')
+    def _compute_experience_years(self):
+        for rec in self:
+            if rec.joining_date:
+                rec.experience_years = (date.today() - rec.joining_date).days // 365
+            else:
+                rec.experience_years = 0
