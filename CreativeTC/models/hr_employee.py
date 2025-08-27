61a2e944480bf15c6415f3dd66a137931e824464
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-27 16:10:24 +0400

    fix
    fi

--- CreativeTC/models/hr_employee.py
+++ CreativeTC/models/hr_employee.py
@@ -48,7 +48,10 @@ class Employee(models.Model):
         ('button2', 'Button 2 - UAE'),
     ], string="Location:")
 
-    dependent_choice = fields.Selection(string="Client:")
+    dependent_choice = fields.Selection(
+        selection=[],
+        string="Client:"
+    )
 
     @api.onchange('main_choice')
     def _onchange_main_choice(self):
