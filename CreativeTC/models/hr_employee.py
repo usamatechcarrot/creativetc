313f7c244c427ad61ae743f0dadbb19696ccd873
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-28 08:27:38 +0400

    fix 1
    fix 1

--- CreativeTC/models/hr_employee.py
+++ CreativeTC/models/hr_employee.py
@@ -11,25 +11,11 @@ class Employee(models.Model):
  # You can type the employee's joining date here
     joining_date = fields.Date(string="Joining Date")
 
-    #   # Auto-computed in years from joining_date to today
-    # experience_years = fields.Integer(
-    #     string="Experience (Years)",
-    #     compute="_compute_experience_years",
-    #     store=True  # set to False if you want it to always compute on the fly
-    # )
-
     experience_text = fields.Char(
         string="Experience",
         compute="_compute_experience_text",
         store=True
     )
-    # @api.depends('joining_date')
-    # def _compute_experience_years(self):
-    #     for rec in self:
-    #         if rec.joining_date:
-    #             rec.experience_years = (date.today() - rec.joining_date).days // 365
-    #         else:
-    #             rec.experience_years = 0
 
     @api.depends('joining_date')
     def _compute_experience_text(self):
