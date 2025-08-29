f7d5a9e2eec4b5b0370248159a2976e9c0e04f91
dineshtechcarrot <dinesh.rampal@techcarrot.ae>
2025-08-29 12:32:55 +0400

    hh

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -9,7 +9,7 @@
         'views/stock_location_views.xml',
         'views/product_views.xml',
         'views/todo_task_views.xml',
-       # 'views/hr_employee_views.xml',
+        'views/hr_employee_views.xml',
      ],
     'installable': True,
     'application': False,
