d4380ddfca762908c6bf003d888b35dc8a7e05a1
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-29 14:30:28 +0400

    234

--- CreativeTC/models/daily_habit_tracker.py
+++ CreativeTC/models/daily_habit_tracker.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 class DailyHabitTracker(models.Model):
-    _name = 'creative.daily_habit_tracker'
+    _name = 'creativetc.daily_habit_tracker'
     _description = 'Daily Habit Tracker'
 
     name = fields.Char(string="Habit", default="Hello Habit")
