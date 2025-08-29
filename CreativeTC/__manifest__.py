ab9a13c6d6e581a089d19babd857795e50e1a083
lalit-suryawanshi <Lalitkumar.sunil@techcarrot.ae>
2025-08-29 16:00:32 +0400

    123123

--- CreativeTC/__manifest__.py
+++ CreativeTC/__manifest__.py
@@ -1,16 +1,12 @@
 {
     'name': 'Real Estate',
-    'version': '18.0',
+    'version': '1.0',
     'category': 'Real Estate',
-    'summary': 'Manage properties, buyers, and sales',
-    'description': 'A custom real estate app to manage properties, clients, and deals.',
-    'author': 'Lalit',
+    'summary': 'Manage Real Estate Properties',
     'depends': ['base'],
     'data': [
-        'views/real_estate_menu.xml',
         'views/property_views.xml',
     ],
     'installable': True,
     'application': True,
-    'auto_install': False,
 }
